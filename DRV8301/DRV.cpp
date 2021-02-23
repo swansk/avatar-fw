ce2a69e2ca1a31d6215cd299cc05a9f75fce4e45
Spencer <sochin.s@husky.neu.edu>
2021-02-22 22:03:21 -0500

    Updated register write and fault functions

--- DRV8301/DRV.cpp
+++ DRV8301/DRV.cpp
@@ -3,7 +3,7 @@
 
 DRV8301::DRV8301(SPI *spi, DigitalOut *cs)
 {
-    /// TODO: verify the spi timings are the same
+    /// TODO: verify the spi mode is the same (probably is)
     _spi = spi;
     _cs = cs;
     _cs->write(1);
@@ -14,6 +14,7 @@ DRV8301::DRV8301(SPI *spi, DigitalOut *cs)
 
 uint16_t DRV8301::spi_write(uint16_t val)
 {
+    /// TODO: verify the spi timings for new chip
     _cs->write(0);
     wait_us(10);
     uint16_t reply = _spi->write(val);
@@ -69,106 +70,68 @@ void DRV8301::print_faults(void)
 
     if (val1 & (1 << 9))
     {
-        printf("VDS_OCP\n\r");
+        printf("GVDD_UV\n\r");
     }
     if (val1 & (1 << 8))
     {
-        printf("GDF\n\r");
+        printf("PVDD_UV\n\r");
     }
     if (val1 & (1 << 7))
     {
-        printf("UVLO\n\r");
+        printf("OTSD\n\r");
     }
     if (val1 & (1 << 6))
     {
-        printf("OTSD\n\r");
+        printf("OTW\n\r");
     }
     if (val1 & (1 << 5))
     {
-        printf("VDS_HA\n\r");
+        printf("FETHA_OC\n\r");
     }
     if (val1 & (1 << 4))
     {
-        printf("VDS_LA\n\r");
+        printf("FETLA_OC\n\r");
     }
     if (val1 & (1 << 3))
     {
-        printf("VDS_HB\n\r");
+        printf("FETHB_OC\n\r");
     }
     if (val1 & (1 << 2))
     {
-        printf("VDS_LB\n\r");
+        printf("FETLB_OC\n\r");
     }
     if (val1 & (1 << 1))
     {
-        printf("VDS_HC\n\r");
+        printf("FETHC_OC\n\r");
     }
     if (val1 & (1))
     {
-        printf("VDS_LC\n\r");
+        printf("FETLC_OC\n\r");
     }
 
-    if (val2 & (1 << 10))
-    {
-        printf("SA_OC\n\r");
-    }
-    if (val2 & (1 << 9))
-    {
-        printf("SB_OC\n\r");
-    }
-    if (val2 & (1 << 8))
-    {
-        printf("SC_OC\n\r");
-    }
     if (val2 & (1 << 7))
     {
-        printf("OTW\n\r");
+        printf("GVDD_OV\n\r");
     }
-    if (val2 & (1 << 6))
-    {
-        printf("CPUV\n\r");
-    }
-    if (val2 & (1 << 5))
-    {
-        printf("VGS_HA\n\r");
-    }
-    if (val2 & (1 << 4))
-    {
-        printf("VGS_LA\n\r");
-    }
-    if (val2 & (1 << 3))
-    {
-        printf("VGS_HB\n\r");
-    }
-    if (val2 & (1 << 2))
-    {
-        printf("VGS_LB\n\r");
-    }
-    if (val2 & (1 << 1))
-    {
-        printf("VGS_HC\n\r");
-    }
-    if (val2 & (1))
-    {
-        printf("VGS_LC\n\r");
-    }
+    int device_ID = val2 & 0xF;
+    printf("Device ID: %d", device_ID);
 }
 
-/// TODO: verify if these are needed or 8301 has these registers
-void DRV8301::enable_gd(void)
-{
-    uint16_t val = (read_register(DCR)) & (~(0x1 << 2));
-    write_register(DCR, val);
-}
+/// TODO: determine if we ever need to disable/enable gate drive faults or if we can delete this
+// void DRV8301::enable_gd(void)
+// {
+//     uint16_t val = (read_register(DCR)) & (~(0x1 << 2));
+//     write_register(DCR, val);
+// }
 
-void DRV8301::disable_gd(void)
-{
-    uint16_t val = (read_register(DCR)) | (0x1 << 2);
-    write_register(DCR, val);
-}
+// void DRV8301::disable_gd(void)
+// {
+//     uint16_t val = (read_register(DCR)) | (0x1 << 2);
+//     write_register(DCR, val);
+// }
 
 void DRV8301::calibrate(void)
 {
-    uint16_t val = 0x1 << 4 + 0x1 << 3 + 0x1 << 2;
-    write_register(CSACR, val);
+    uint16_t val = 0x1 << 5 + 0x1 << 4;
+    write_register(CR2, val);
 }
