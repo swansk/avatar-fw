71fb592fca0b28046762481ecbc7b822c7c97672
Spencer <sochin.s@husky.neu.edu>
2021-03-03 13:23:41 -0500

    testing interrupts and threads

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,50 @@
-#include <mbed.h>
+#include "mbed.h"
 
-DigitalOut led1(LED1);
-DigitalOut led2(LED2);
+DigitalOut led1(LED1); // orange
+DigitalOut led2(LED2); // green
+DigitalOut led5(LED5); // red
+DigitalOut led6(LED6); // blue
+Thread thread1;
+Thread thread2;
+InterruptIn button(BUTTON1);
 
-int main() {
+void int_routine()
+{
+  led1 = 0;
+  led2 = 0;
+  led5 = 0;
+  led6 = 0;
+  wait_us(1000000);
+}
 
-  // put your setup code here, to run once:
-  while(1) {
+void led1_thread()
+{
+  while (true)
+  {
     led1 = !led1;
-    thread_sleep_for(1000);
+    ThisThread::sleep_for(500);
+  }
+}
+
+void led2_thread()
+{
+  while (true)
+  {
+    led2 = !led2;
+    ThisThread::sleep_for(750);
+  }
+}
+
+int main()
+{
+  // put your setup code here, to run once:
+  button.fall(&int_routine);
+  thread1.start(led1_thread);
+  thread2.start(led2_thread);
+  while (1)
+  {
+    led5 = !led5;
+    led6 = !led6;
+    ThisThread::sleep_for(1000);
   }
 }
